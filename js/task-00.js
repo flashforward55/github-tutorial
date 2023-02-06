68faf7d5d6f4e3ca173cc62ca3798dc221b1ffde
Anton Styazhkin <rudana089@gmail.com>
2023-02-06 16:01:43 +0200

    Update

--- js/task-00.js
+++ js/task-00.js
@@ -1,11 +1,16 @@
-function calculateTotalPrice(order) {
-    let total = 0;
-    // Change code below this line
-    for (let num of order) {
-        total += num;
-    }
-    // Change code above this line
-    return console.log(total);
-}
+const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
 
-calculateTotalPrice([412, 371, 94, 63, 176]);
+const findLogin = function (allLogins, loginToFind) {
+    for (const login of allLogins) {
+        if (login === loginToFind) {
+            return `Пользователь ${loginToFind} найден.`;
+        }
+    }
+
+    return `Пользователь ${loginToFind} не найден.`;
+};
+
+console.log(findLogin(logins, 'avocod3r'));
+console.log(findLogin(logins, 'k1widab3st'));
+console.log(findLogin(logins, 'jam4l'));
+console.log(findLogin(logins, 'poly1scute'));
