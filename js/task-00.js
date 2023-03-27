6d8df90714964555acca8f0f29fe24567c39e0b3
Anton Styazhkin <rudana089@gmail.com>
2023-03-27 22:55:10 +0300

    Update task-00.js

--- js/task-00.js
+++ js/task-00.js
@@ -1,5 +1,5 @@
 const apikey = 'c78751ad2a1b6968019e8b5d53258129';
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apikey}&units=metric`;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kryvyi Rih&appid=${apikey}&units=metric`;
 
 fetch(apiUrl)
     .then(response => response.json())
@@ -9,7 +9,7 @@ fetch(apiUrl)
             const weatherTemp = document.querySelector('.weather-temp');
             const weatherDescription = document.querySelector('.weather-description');
             const weatherCity = document.querySelector('.weather-city');
-
+            console.log(data);
             weatherIcon.style.backgroundImage = `url('https://openweathermap.org/img/w/${data.weather[0].icon}.png')`;
             weatherTemp.innerHTML = `${Math.round(data.main.temp)}<sup>°C</sup>`;
             weatherDescription.innerHTML = data.weather[0].description;
